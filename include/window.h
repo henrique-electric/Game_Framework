16e06810c16912d4371f35485cc2610f7fc92989
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-10-03 21:49:07 -0300

    add include dir

---
+++ include/window.h
@@ -0,0 +1,10 @@
+//
+//  window.hpp
+//  Game
+//
+//  Created by BaianoDaBahia on 03/10/25.
+//
+
+#pragma once
+#include <SDL/SDL3.h>
+
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,6 @@
 #include <SDL3/SDL.h>
 
 int main() {
-	fmt::print("Hi");
+	
 	return 0;
 }
